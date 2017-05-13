52e781427d5c173f6f36e50e829c72f2979b6c6c
Shahakbk <shahakbk@gmail.com>
2017-05-13 14:27:16 +0300

    updated dry ex, changed realloc to malloc and free  + checked with valgrind

--- hw2dry.c
+++ hw2dry.c
@@ -4,25 +4,36 @@
 
 char *flat_text(char **words, int n) {
     char *result = NULL;
+    char *tmp_result = NULL;
     int total_length = 0;
     for (int i = 0; i < n; ++i) {
         int length = (int)strlen(words[i]);
         total_length += length;
-        result = realloc(result, (size_t) total_length + 1);
-        if (result == NULL) {
+        tmp_result = malloc((size_t) total_length + 1);
+        if (tmp_result == NULL) {
+            free(result);
             return NULL;
         }
-        strcpy(result+ (total_length - length), words[i]);
+        if (result != NULL) {
+            strcpy(tmp_result, result);
+        }
+        strcpy(tmp_result + (total_length - length), words[i]);
+        free(result);
+        result = tmp_result;
     }
     return result;
 }
 
-int main() {
-    char *words[4];
+void initialize_words_arr (char **words) {
     words[0] = "Hello";
     words[1] = "To";
     words[2] = "234122";
     words[3] = "Matam";
+}
+
+int main() {
+    char *words[4];
+    initialize_words_arr(words);
     char *p = flat_text(words, 4);
     if (p!=NULL)
     {
